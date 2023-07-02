cf82112eef63da12f3f89ed93a20c6f58e57b1f4
Apple <apple@Apples-MacBook-Pro.local>
2023-07-03 00:16:31 +0545

    BITGO

--- locator.py
+++ locator.py
@@ -1,12 +1,10 @@
 from selenium.webdriver.common.by import By
 
 # https://blockstream.info/block/000000000000000000076c036ff5119e5a5a74df77abf64203473364509f7732
-# Below the block details div, you will see the transaction list section.
-driver = None
-
+# Below the block details div, you will see the transaction list section
 
 class WebLocators(object):
-    BLOCK_DETAILS = (By.CSS_SELECTOR, "div.transactions>h3")
-    TRANSACTION_ID = (By.CSS_SELECTOR, ".txn>a")
-    TRANSACTION_LIST_INPUT = (By.CSS_SELECTOR, ".transaction-box>div.ins-and-outs>div.vins")
-    TRANSACTION_LIST_OUTPUT = (By.CSS_SELECTOR, ".transaction-box>div.ins-and-outs>div.vouts")
+    BLOCK_DETAILS = "div.transactions>h3"
+    TRANSACTION_IDS = ".txn>a"
+    TRANSACTION_LIST_INPUTS = ".transaction-box>div.ins-and-outs>div.vins"
+    TRANSACTION_LIST_OUTPUTS = ".transaction-box>div.ins-and-outs>div.vouts"
--- tests.py
+++ tests.py
@@ -22,8 +22,8 @@ class TestTransactionPage:
             transaction_url = "https://blockstream.info/block/000000000000000000076c036ff5119e5a5a74df77abf64203473364509f7732"
             chrome_driver_path = 'usr/local/bin/chromedriver'
             # '/path/to/chromedriver'
-            driver = webdriver.Chrome(chrome_driver_path, options=chrome_options)
-            driver.get(transaction_url)
+            self.driver = webdriver.Chrome(chrome_driver_path, options=chrome_options)
+            self.driver.get(transaction_url)
         except WebDriverException as e:
             print("An error occurred while opening the URL:", e)
 
@@ -32,15 +32,15 @@ class TestTransactionPage:
     @allure.description("Validate the section has the heading 25 of 2875 Transactions")
     def test_transaction(self):
         expected_text = "25 of 2875 Transactions"
-        elem_text = WebLocators.BLOCK_DETAILS.text
+        elem_text = self.driver.find_element(WebLocators.BLOCK_DETAILS).text
         assert elem_text == expected_text, f"Element text '{elem_text}' does not match expected text '{expected_text}'"
 
     @allure.title("Transaction Test 2")
     @allure.description("Parse & Validate the 1 in 2 Op, transaction")
     def test_homepage(self):
-        t_id = WebLocators.TRANSACTION_ID
-        transaction_in_elements = WebLocators.TRANSACTION_LIST_INPUT
-        transaction_out_elements = WebLocators.TRANSACTION_LIST_OUTPUT
+        t_id = self.driver.find_elements(WebLocators.TRANSACTION_ID)
+        transaction_in_elements = self.driver.find_elements(WebLocators.TRANSACTION_LIST_INPUT)
+        transaction_out_elements = self.driver.find_elements(WebLocators.TRANSACTION_LIST_OUTPUT)
         hashMap = {}
 
         #     First the total elements inside the vins will be found using the size of the elements inside vins,
@@ -63,10 +63,10 @@ class TestTransactionPage:
             if value == (1, 2):
                 print("Found (1,2) pairing for Transaction id:", hashMap[key])
                 assert True
-                driver.quit()
+                self.driver.quit()
             else:
                 assert False
-                driver.quit()
+                self.driver.quit()
 
     def tearDown(self):
         self.driver.close()
