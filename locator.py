71d48b3602d95a03c776bd72b2919546d477fe9d
Apple <apple@Apples-MacBook-Pro.local>
2023-07-03 02:12:19 +0545

    BITGO

--- locator.py
+++ locator.py
@@ -1,11 +1,30 @@
-from selenium.webdriver.common.by import By
-
-
-# https://blockstream.info/block/000000000000000000076c036ff5119e5a5a74df77abf64203473364509f7732
-# Below the block details div, you will see the transaction list section
-
+# from selenium.webdriver.common.by import By
+#
+#
+# # https://blockstream.info/block/000000000000000000076c036ff5119e5a5a74df77abf64203473364509f7732
+# # Below the block details div, you will see the transaction list section
+#
 class WebLocators:
     BLOCK_DETAILS = "div.transactions>h3"
     TRANSACTION_IDS = ".txn>a"
     TRANSACTION_LIST_INPUTS = ".transaction-box>div.ins-and-outs>div.vins"
     TRANSACTION_LIST_OUTPUTS = ".transaction-box>div.ins-and-outs>div.vouts"
+
+# hashmap = {}
+#
+# # Example key-value pairs
+# obj1 = "Value 1"
+# obj2 = "Value 2"
+# obj3 = "Value 3"
+#
+# # Assigning values to the hashmap
+# hashmap[id(obj1)] = ("Tuple 1", "String 1")
+# hashmap[id(obj2)] = ("Tuple 2", "String 2")
+# hashmap[id(obj3)] = ("Tuple 3", "String 3")
+#
+# # Printing the elements of the hashmap
+# for key, value in hashmap.items():
+#     print(f"ID: {key}")
+#     print(f"Tuple Value: {value[0]}")
+#     print(f"String Value: {value[1]}")
+#     print()
--- tests.py
+++ tests.py
@@ -3,80 +3,84 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import WebDriverException
+from selenium.common.exceptions import NoSuchElementException
 import allure
 
 from locator import WebLocators
+from util import chromeSession
 
 
-# @pytest.mark.smoke()
 class TestTransactionPage:
     @allure.title("setup")
     @allure.description("Test setup")
-    def __init__(self):
+    def setUp(self):
+        # self.driver = webdriver.Chrome()
         try:
-            chrome_options = Options()
-            chrome_options.add_argument('--headless')
-            chrome_options.add_argument('--disable-gpu')
-            chrome_options.add_argument("--window-size=1920x1080")
-            chrome_options.add_argument("--verbose")
-            transaction_url = "https://blockstream.info/block" \
-                              "/000000000000000000076c036ff5119e5a5a74df77abf64203473364509f7732"
-            chrome_driver_path = 'usr/local/bin/chromedriver'
-            # '/path/to/chromedriver'
-
-            driver = webdriver.Chrome(chrome_driver_path, options=chrome_options)
-            driver.get(transaction_url)
+            self.driver = webdriver.Chrome(options=chromeSession.chrome_options)
+            self.driver.get(chromeSession.transaction_url)
         except WebDriverException as e:
             print("An error occurred while opening the URL:", e)
 
-    # @pytest.fixture(scope='session')
     @allure.title("Transaction Test 1")
     @allure.description("Validate the section has the heading 25 of 2875 Transactions")
     def test_transaction(self):
-        expected_text = "25 of 2875 Transactions"
-        elem_text = self.driver.find_element(WebLocators.BLOCK_DETAILS).text
-        print(elem_text)
-        assert elem_text == expected_text, f"Element text '{elem_text}' does not match expected text '{expected_text}'"
-
+        try:
+            expected_text = "25 of 2875 Transactions"
+            elem = self.driver.find_element(By.CSS_SELECTOR, str(WebLocators.BLOCK_DETAILS))
+            elem_text = elem.text
+            print(elem_text)
+            assert elem_text == expected_text, f"Element text '{elem_text}' does not match expected text '{expected_text}'"
+        except NoSuchElementException:
+            print("Element not found on the page.")
+        finally:
+            self.driver.quit()
     @allure.title("Transaction Test 2")
     @allure.description("Parse & Validate the 1 in 2 Op, transaction")
     def test_homepage(self):
-        t_id = self.driver.find_elements(WebLocators.TRANSACTION_ID)
-        transaction_in_elements = self.driver.find_elements(WebLocators.TRANSACTION_LIST_INPUT)
-        transaction_out_elements = self.driver.find_elements(WebLocators.TRANSACTION_LIST_OUTPUT)
-        hashMap = {}
+        hashmap = {}
+        try:
+            t_id = self.driver.find_elements(By.CSS_SELECTOR, str(WebLocators.TRANSACTION_IDS))
+            transaction_in_elements = self.driver.find_elements(By.CSS_SELECTOR, str(WebLocators.TRANSACTION_LIST_INPUTS))
+            transaction_out_elements = self.driver.find_elements(By.CSS_SELECTOR, str(WebLocators.TRANSACTION_LIST_OUTPUTS))
+            tid_size = []
+            for el in t_id:
+                tid_size.append(el.text)
+                # key = tid_size[el]
+            tin_size = []
+            for el in transaction_in_elements:
+                tin_size.append(el.size)
+            tout_size = []
+            for el in transaction_out_elements:
+                tout_size.append(el.size)
 
-        #     First the total elements inside the vins will be found using the size of the elements inside vins,
-        #     we save the value for each length of element in a hash map. There will be a loop for 25 transactions,
-        #     each loop will generate the size of the vins div. Similarly the same will be done for the vouts and saving
-        #     the output in the same hashmap
-        # For each transaction, the transaction hash can also be saved in the same hashmap when the loop is running
-        # At the end of the loop, the hash map that corresponds to a 1:2 ins and outs will be the one we need to print
-        # Print out the transaction id for that on console
-
-        for i in list(range(25)):
-            key = t_id[i].text
-            t_in = transaction_in_elements[i].size
-            t_out = transaction_out_elements[i].size
-            value = (t_in, t_out)
-            hashMap[key] = value
-            print(hashMap[key, value])
-
-        for key, value in hashMap.items():
-            if value == (1, 2):
-                print("Found (1,2) pairing for Transaction id:", hashMap[key])
+            for i in list(range(25)):
+             hashmap[id(tid_size[i])] = (tin_size[i],tout_size[i])
+        except NoSuchElementException:
+            print("Element not found on the page.")
+        """
+                    First the total elements inside the vins will be found using the size of the elements inside vins,
+                    we save the value for each length of element in a hash map. There will be a loop for 25 transactions,
+                    each loop will generate the size of the vins div. Similarly the same will be done for the vouts and saving
+                    the output in the same hashmap
+                For each transaction, the transaction hash can also be saved in the same hashmap when the loop is running
+                At the end of the loop, the hash map that corresponds to a 1:2 ins and outs will be the one we need to print
+                Print out the transaction id for that on console
+        """
+        for key, value in hashmap.items():
+            if {value[0]} == 1 & {value[1]} == 2:
+                print("Found (1,2) pairing for Transaction id:", hashMap[{key}])
                 assert True
-                self.driver.quit()
             else:
                 assert False
                 self.driver.quit()
 
-    def tearDown(self):
+    def teardown(self):
         self.driver.close()
 
 
 if __name__ == "__main__":
     test = TestTransactionPage()
+    test.setUp()
     test.test_transaction()
     test.test_homepage()
-    test.tearDown()
+    # test.tearDown()
