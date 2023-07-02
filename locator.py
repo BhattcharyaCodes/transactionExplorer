fd6256bd078d5af018fa13490703a3505ddb7a29
Apple <apple@Apples-MacBook-Pro.local>
2023-07-02 16:07:32 +0545

    BitGO

--- locator.py
+++ locator.py
@@ -1,8 +1,6 @@
 from selenium.webdriver.common.by import By
-driver = None
 # https://blockstream.info/block/000000000000000000076c036ff5119e5a5a74df77abf64203473364509f7732
 # Below the block details div, you will see the transaction list section.
-# div.transactions>h3
 
 BLOCK_DETAILS = driver.find_element(By.CSS_SELECTOR, "div.transactions>h3")
 TRANSACTION_ID = driver.find_elements(By.CSS_SELECTOR, ".txn>a")
