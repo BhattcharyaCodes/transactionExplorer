c50e5c9bfe4b07e089c6c7a0843d4c9f6bb1caf9
Apple <apple@Apples-MacBook-Pro.local>
2023-07-03 00:17:36 +0545

    BITGO

--- locator.py
+++ locator.py
@@ -1,9 +1,10 @@
 from selenium.webdriver.common.by import By
 
+
 # https://blockstream.info/block/000000000000000000076c036ff5119e5a5a74df77abf64203473364509f7732
 # Below the block details div, you will see the transaction list section
 
-class WebLocators(object):
+class WebLocators:
     BLOCK_DETAILS = "div.transactions>h3"
     TRANSACTION_IDS = ".txn>a"
     TRANSACTION_LIST_INPUTS = ".transaction-box>div.ins-and-outs>div.vins"
